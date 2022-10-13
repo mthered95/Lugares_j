b48a1957692981cbf856fdd9723862a3c98a6ab8
Mario Rojas Leiva <94200810+mthered95@users.noreply.github.com>
2022-10-13 12:12:15 -0600

    Commit 4 de Clases Jueves

--- app/src/main/java/com/example/lugares_j/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/example/lugares_j/ui/lugar/AddLugarFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.lugares_j.R
 import com.example.lugares_j.databinding.FragmentAddLugarBinding
 import com.example.lugares_j.databinding.FragmentLugarBinding
+import com.example.lugares_j.model.Lugar
 import com.example.lugares_j.viewmodel.LugarViewModel
 
 class AddLugarFragment : Fragment() {
 
+    private lateinit var lugarViewModel: LugarViewModel
     private var _binding: FragmentAddLugarBinding? = null
     private val binding get() = _binding!!
 
@@ -19,14 +24,41 @@ class AddLugarFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(LugarViewModel::class.java)
+        lugarViewModel = ViewModelProvider(this).get(LugarViewModel::class.java)
 
         _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
 
+        binding.btAdd.setOnClickListener { addLugar() }
+
+
         return binding.root
     }
+
+    private fun addLugar() {
+        val nombre =
+            binding.etNombre.text.toString() // se obtiene el texto de lo que escribe el usuario
+        if (nombre.isNotEmpty()) { // si se escribe algo en el nombre se puede guardar
+            val correo = binding.etCorreo.text.toString() //se obtiene el texto de lo que escribe el usuario
+            val telefono = binding.etTelefono.text.toString() //se obtiene el texto de lo que escribe el usuario
+            val web = binding.etWeb.text.toString() //se obtiene el texto de lo que escribe el usuario
+            val lugar = Lugar(0,nombre, correo, telefono, web,
+                0.0, 0.0,0.0,"", "")
+
+            // se procede a registrar el nuevo lugar
+            lugarViewModel.saveLugar(lugar)
+            Toast.makeText(requireContext(),
+                getString(R.string.msg_lugar_added),
+                Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        } else{ //no se puede registrar falta info
+            Toast.makeText(requireContext(),
+                getString(R.string.msg_data),
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/lugares_j/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/example/lugares_j/ui/lugar/LugarFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lugares_j.R
+import com.example.lugares_j.adapter.LugarAdapter
 import com.example.lugares_j.databinding.FragmentLugarBinding
 import com.example.lugares_j.viewmodel.LugarViewModel
 
@@ -22,13 +24,23 @@ class LugarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(LugarViewModel::class.java)
+        val lugarViewModel =
+            ViewModelProvider(this)[(LugarViewModel::class.java)]
 
         _binding = FragmentLugarBinding.inflate(inflater, container, false)
 
         binding.addLugarFabButton.setOnClickListener{
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+        // se genera el reciclear view para ver la información
+        val lugarAdapter=LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = lugarAdapter // es quien reemplaza la info
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel.getLugares.observe(viewLifecycleOwner){
+            lugares -> lugarAdapter.setListaLugares(lugares)
         }
 
         return binding.root
